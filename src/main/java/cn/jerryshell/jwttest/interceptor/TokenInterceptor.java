698c1eac6416ae76755ea060b6f3efca7e08e1e1
Jerry <calljerryli@outlook.com>
2018-10-16 21:26:48 +0800

    简化代码，修改注释

--- src/main/java/cn/jerryshell/jwttest/interceptor/TokenInterceptor.java
+++ src/main/java/cn/jerryshell/jwttest/interceptor/TokenInterceptor.java
@@ -7,24 +7,16 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Method;
 
 public class TokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // 如果不是映射到方法直接通过
-        if (!(handler instanceof HandlerMethod)) {
-            return true;
-        }
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        Method method = handlerMethod.getMethod();
-
-        // 判断接口是否需要登录
-        TokenRequired methodAnnotation = method.getAnnotation(TokenRequired.class);
+        // 判断 handler 是否有 TokenRequired 注解，如果没有则拦截器直接放行
+        TokenRequired methodAnnotation = ((HandlerMethod) handler).getMethodAnnotation(TokenRequired.class);
         if (methodAnnotation == null) {
             return true;
         }
-
+        // 校验 token
         String token = request.getHeader("Authorization");
         String username = JWTUtil.getUsername(token);
         boolean verify = JWTUtil.verify(token, username);
