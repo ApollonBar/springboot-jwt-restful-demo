d5d4b02bd145dbed2ca72fd441e414b3b23481aa
Jerry <calljerryli@outlook.com>
2018-10-27 20:43:44 +0800

    修改：使用 lombok 简化 getter 和 setter 代码

--- src/main/java/cn/jerryshell/jwtrestful/domain/User.java
+++ src/main/java/cn/jerryshell/jwtrestful/domain/User.java
@@ -1,40 +1,31 @@
 package cn.jerryshell.jwtrestful.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
     private String username;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
     private String role;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
